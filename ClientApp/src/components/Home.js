93da48188273057127d06cd24f9dab57f667a643
Craig Hughes <hghscraig@gmail.com>
2020-02-29 19:58:58 +0000

    Created basic PostsController with GET request and added populate and render functions to Home.js to show all posts

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: null,
+            loading: true
+        };
+    }
+
+    /**
+     *  On load, populate posts to then render.
+     * */
+    componentDidMount() {
+        this.populatePosts();
+    }
+
+    /**
+     *  GET Requests to populate posts state. Sets loading state to false on
+     *  completion.
+     * */
+    async populatePosts() {
+
+        const response = await fetch("Posts", {
+            method: 'GET'
+        }).then(res => res.json());
+
+        const data = await response.posts;
+        console.log(data);
+        this.setState({ posts: data, loading: false });
+    }
+
+    /**
+     * Render elements to fill body content.
+     * @param {Array} posts
+     */
+    renderPostsElements(posts) {
+
+        // Let user know there are no posts.
+        if (posts.length < 1) {
+            return <p>No posts to show</p>;
+        }
+
+        return (
+            <section>
+                {posts.map(post => {
+                    return <article key={post.Id}>
+                        <p>{post.Title}</p>
+                        <p>`Posted at: ${post.PostedAt} by ${post.Author}`</p>
+                        <p>{post.Description}</p>
+                        <p>{post.Body}</p>
+                    </article>
+                })}
+            </section>
+        );
+    }
+
+
+    render() {
+
+        let content = this.state.loading ? <p>Loading...</p> :
+            this.renderPostsElements(this.state.posts);
+
+        return (
+            <main>
+                <h1>Hello, world!</h1>
+                {content}
+            </main>
+        );
+    }
 }
---
+++ Controllers/PostsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestBlog.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestBlog.Controllers
+{
+    [ApiController]
+    [Route("Posts")]
+    public class PostsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PostsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _db.Posts.OrderByDescending(x => x.Id).ToArray();
+            return Ok(new { posts = posts });
+        }
+    }
+}
